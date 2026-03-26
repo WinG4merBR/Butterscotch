9a65fab5292b940409d8066a6498bdd95a92fcfc
Fancy2209 <64917206+Fancy2209@users.noreply.github.com>
2026-03-26 18:56:45 -0100

    Make gsDrawLineColor use the 2 colors

--- src/ps2/gs_renderer.c
+++ src/ps2/gs_renderer.c
@@ -1196,9 +1196,29 @@ static void gsDrawLine(Renderer* renderer, float x1, float y1, float x2, float y
     gs->zCounter++;
 }
 
-// PS2 gsKit doesn't support per-vertex colors on lines, so we just use color1
-static void gsDrawLineColor(Renderer* renderer, float x1, float y1, float x2, float y2, float width, uint32_t color1, [[maybe_unused]] uint32_t color2, float alpha) {
-    renderer->vtable->drawLine(renderer, x1, y1, x2, y2, width, color1, alpha);
+static void gsDrawLineColor(Renderer* renderer, float x1, float y1, float x2, float y2, float width, uint32_t color1, uint32_t color2, float alpha) {
+    GsRenderer* gs = (GsRenderer*) renderer;
+
+    uint8_t a = alphaToGS(alpha);
+
+    uint8_t r1 = BGR_R(color1);
+    uint8_t g1 = BGR_G(color1);
+    uint8_t b1 = BGR_B(color1);
+
+    uint8_t r2 = BGR_R(color2);
+    uint8_t g2 = BGR_G(color2);
+    uint8_t b2 = BGR_B(color2);
+
+    u64 lineColor1 = GS_SETREG_RGBAQ(r1, g1, b1, a, 0x00);
+    u64 lineColor2 = GS_SETREG_RGBAQ(r2, g2, b2, a, 0x00);
+
+    float sx1 = (x1 - (float) gs->viewX) * gs->scaleX + gs->offsetX;
+    float sy1 = (y1 - (float) gs->viewY) * gs->scaleY + gs->offsetY;
+    float sx2 = (x2 - (float) gs->viewX) * gs->scaleX + gs->offsetX;
+    float sy2 = (y2 - (float) gs->viewY) * gs->scaleY + gs->offsetY;
+
+    gsKit_prim_line_goraud(gs->gsGlobal, sx1, sy1, sx2, sy2, gs->zCounter, lineColor1, lineColor2);
+    gs->zCounter++;
 }
 
 static void gsDrawText(Renderer* renderer, const char* text, float x, float y, float xscale, float yscale, [[maybe_unused]] float angleDeg) {
