f7d7010f93bcfb662191edc1b9861a3a3c453185
Fancy2209 <paulo.medeiros1302@gmail.com>
2026-04-29 22:59:15 +0000

    I am stoopid

--- src/ps3/main.c
+++ src/ps3/main.c
@@ -293,17 +293,18 @@ int main(int argc, char* argv[]) {
         ps3glSwapBuffers();
 
         // Limit frame rate to room speed (skip in headless mode for max speed!!)
-        if (runner->currentRoom->speed < 0) {
-            double targetFrameTime = 1.0 / 30;//(runner->currentRoom->speed);
+        if (runner->currentRoom->speed > 0) {
+            double targetFrameTime = 1.0 / (runner->currentRoom->speed);
             double nextFrameTime = lastFrameTime + targetFrameTime;
+
+            // Sleep for most of the remaining time, then spin-wait for precision
+            double remaining = nextFrameTime - PS3_GET_TIME;
+            if (remaining > 0.002) {
+                sysUsleep((uint32_t)((remaining - 0.001) * 1000000.0));
+            }
+
             while (PS3_GET_TIME < nextFrameTime) {
                 // Spin-wait for the remaining sub-millisecond
-
-                // Sleep for most of the remaining time, then spin-wait for precision
-                double remaining = nextFrameTime - PS3_GET_TIME;
-                if (remaining > 0.002) {
-                    sysUsleep((uint32_t)((remaining - 0.001) * 1000000.0));
-                }
             }
             lastFrameTime = nextFrameTime;
         } else {
